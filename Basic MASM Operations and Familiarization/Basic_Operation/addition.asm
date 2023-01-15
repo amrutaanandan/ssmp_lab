199039431d1e36313db570892c7c0573c6ced538
Amruta Anandan <76481299+amrutaanandan@users.noreply.github.com>
2023-01-15 21:12:29 +0530

    Create addition.asm

---
+++ Basic MASM Operations and Familiarization/Basic_Operation/addition.asm
@@ -0,0 +1,42 @@
+;MASM program for 16-bit addition 
+
+.model small
+.data
+.stack
+.code
+mov ax, @data
+mov ds, ax
+mov ax, 1234h
+mov bx, 7779h
+add ax, bx
+
+mov cx, 04h
+mov dx, 0h
+item: mov bx, 10h
+div bx
+push dx
+dec cx
+cmp cx, 0h
+je update
+xor dx, dx
+;clear dx
+jmp item
+update: mov cx, 4
+print1: cmp cx, 0
+je exit
+pop dx
+cmp dx, 0ah
+jge alpha
+add dx, 48
+jmp incre
+alpha: add dx, 55
+;55 in decimal
+incre: mov ah, 02h
+int 21h
+dec cx
+jmp print1
+exit: mov ah, 4ch
+int 21h
+.exit
+end
+
